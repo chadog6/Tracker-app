1884c810987d926f803e68d4f3c39b93a1f7d64e
Chase Hughes <101086721+chadog6@users.noreply.github.com>
2022-08-05 19:10:00 -0600

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class app extends React.Component{
+    render() {
+        return(
+            <div>
+                <h1> Whats good</h1>
+            </div>
+        )
+    }
+}
+export default app;
